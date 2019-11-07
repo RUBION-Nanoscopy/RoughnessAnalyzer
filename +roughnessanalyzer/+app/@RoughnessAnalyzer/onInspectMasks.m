ecec3a84695485b6fbe63ffba42e6baa0ab97dd6
patta42 <patrick.happel@rub.de>
2019-11-07 15:48:26 +0100

    Initial commit

---
+++ +roughnessanalyzer/+app/@RoughnessAnalyzer/onInspectMasks.m
@@ -0,0 +1,33 @@
+function onInspectMasks(self, nscan)
+    data = self.DisplayIntensityImage;
+    
+    switch self.Settings.Display
+        case 'slope'
+            CM = self.SlopeCM;
+            CLim = self.SlopeCLim;
+            
+            % data is one pixel smaller...
+            sz = size(self.Session.Scans{nscan}.SICMScan.zdata_grid);
+            if size(data,1) < sz(1) 
+                data(end+1,:) = data(end,:);
+            elseif size(data,2) < sz(2)
+                data(:, end+1) = data(:, end);
+            end
+            
+        case 'height'
+            CM = self.HeightCM;
+            CLim = self.heightCLim;
+    end
+    
+    rc = roughnessanalyzer.app.MaskComparer(...
+        'Masks', self.Session.Scans{nscan}.Masks, ...
+        'onApply',@(m)local_assign_mask(self, nscan, m), ...
+        'CLim', CLim, 'CM', CM, 'Data', data ...
+    );
+
+    waitfor(rc);
+end
+
+function local_assign_masks(obj, nscan, m)
+
+end
