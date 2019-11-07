3e84007b8aee23ef8518261a86e1d234b70763af
patta42 <patrick.happel@rub.de>
2019-11-07 15:57:40 +0100

    Added Menu Item for Cleaning Masks

--- +roughnessanalyzer/+app/@RoughnessAnalyzer/getScanNodeContextMenu.m
+++ +roughnessanalyzer/+app/@RoughnessAnalyzer/getScanNodeContextMenu.m
@@ -1,4 +1,5 @@
 function cm = getScanNodeContextMenu(self, nscan)
     cm = uicontextmenu(self.Figure);
     uimenu(cm, 'Text', 'Rename Scan', 'Callback', @(~,~) self.onRenameScan(nscan));
+    uimenu(cm, 'Text', 'Inspect Masks', 'Callback', @(~,~) self.onInspectMasks(nscan));
 end
